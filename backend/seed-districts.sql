1f7428cbdfc1310176bd19cc655222d1d9a10396
Jasurbek414 <muminovjasurbek12@gmail.com>
2026-04-24 17:39:13 +0500

    Seed all 194 real districts of Uzbekistan into database

---
+++ backend/seed-districts.sql
@@ -0,0 +1,241 @@
+-- O'zbekiston Respublikasi barcha tuman va shaharlar
+-- Viloyat ID xaritasi:
+-- 1=Toshkent shahri, 2=Toshkent viloyati, 3=Samarqand, 4=Buxoro, 5=Andijon
+-- 6=Farg'ona, 7=Namangan, 8=Qashqadaryo, 9=Surxondaryo, 10=Jizzax
+-- 11=Sirdaryo, 12=Navoiy, 13=Xorazm, 14=Qoraqalpog'iston
+
+-- ═══ 1. TOSHKENT SHAHRI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Bektemir tumani', 1),
+('Chilonzor tumani', 1),
+('Yakkasaroy tumani', 1),
+('Mirobod tumani', 1),
+('Mirzo Ulug''bek tumani', 1),
+('Olmazor tumani', 1),
+('Sergeli tumani', 1),
+('Shayxontohur tumani', 1),
+('Uchtepa tumani', 1),
+('Yashnaobod tumani', 1),
+('Yunusobod tumani', 1),
+('Yangihayot tumani', 1);
+
+-- ═══ 2. TOSHKENT VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Angren shahri', 2),
+('Olmaliq shahri', 2),
+('Chirchiq shahri', 2),
+('Bekobod shahri', 2),
+('Nurafshon shahri', 2),
+('Ohangaron shahri', 2),
+('Bekobod tumani', 2),
+('Bo''stonliq tumani', 2),
+('Bo''ka tumani', 2),
+('Chinoz tumani', 2),
+('Qibray tumani', 2),
+('Ohangaron tumani', 2),
+('Oqqo''rg''on tumani', 2),
+('Parkent tumani', 2),
+('Piskent tumani', 2),
+('Quyi Chirchiq tumani', 2),
+('O''rta Chirchiq tumani', 2),
+('Yangiyo''l tumani', 2),
+('Yuqori Chirchiq tumani', 2),
+('Zangiota tumani', 2),
+('Toshkent tumani', 2);
+
+-- ═══ 3. SAMARQAND VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Samarqand shahri', 3),
+('Kattaqo''rg''on shahri', 3),
+('Bulung''ur tumani', 3),
+('Ishtixon tumani', 3),
+('Jomboy tumani', 3),
+('Kattaqo''rg''on tumani', 3),
+('Narpay tumani', 3),
+('Nurobod tumani', 3),
+('Oqdaryo tumani', 3),
+('Past Darg''om tumani', 3),
+('Payariq tumani', 3),
+('Qo''shrabot tumani', 3),
+('Samarqand tumani', 3),
+('Toyloq tumani', 3),
+('Urgut tumani', 3);
+
+-- ═══ 4. BUXORO VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Buxoro shahri', 4),
+('Kogon shahri', 4),
+('Buxoro tumani', 4),
+('G''ijduvon tumani', 4),
+('Jondor tumani', 4),
+('Kogon tumani', 4),
+('Olot tumani', 4),
+('Peshku tumani', 4),
+('Qorakul tumani', 4),
+('Qorako''l tumani', 4),
+('Romitan tumani', 4),
+('Shofirkon tumani', 4),
+('Vobkent tumani', 4);
+
+-- ═══ 5. ANDIJON VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Andijon shahri', 5),
+('Xonobod shahri', 5),
+('Andijon tumani', 5),
+('Asaka tumani', 5),
+('Baliqchi tumani', 5),
+('Bo''z tumani', 5),
+('Buloqboshi tumani', 5),
+('Izboskan tumani', 5),
+('Jalaquduq tumani', 5),
+('Marhamat tumani', 5),
+('Oltinko''l tumani', 5),
+('Paxtaobod tumani', 5),
+('Qo''rg''ontepa tumani', 5),
+('Shahrixon tumani', 5),
+('Ulug''nor tumani', 5),
+('Xo''jaobod tumani', 5);
+
+-- ═══ 6. FARG'ONA VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Farg''ona shahri', 6),
+('Marg''ilon shahri', 6),
+('Quvasoy shahri', 6),
+('Qo''qon shahri', 6),
+('Bag''dod tumani', 6),
+('Beshariq tumani', 6),
+('Buvayda tumani', 6),
+('Dang''ara tumani', 6),
+('Farg''ona tumani', 6),
+('Furqat tumani', 6),
+('Oltiariq tumani', 6),
+('O''zbekiston tumani', 6),
+('Quva tumani', 6),
+('Rishton tumani', 6),
+('So''x tumani', 6),
+('Toshloq tumani', 6),
+('Uchko''prik tumani', 6),
+('Yozyovon tumani', 6);
+
+-- ═══ 7. NAMANGAN VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Namangan shahri', 7),
+('Chortoq tumani', 7),
+('Chust tumani', 7),
+('Kosonsoy tumani', 7),
+('Mingbuloq tumani', 7),
+('Namangan tumani', 7),
+('Norin tumani', 7),
+('Pop tumani', 7),
+('To''raqo''rg''on tumani', 7),
+('Uychi tumani', 7),
+('Uchqo''rg''on tumani', 7),
+('Yangiqo''rg''on tumani', 7);
+
+-- ═══ 8. QASHQADARYO VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Qarshi shahri', 8),
+('Shahrisabz shahri', 8),
+('Chiroqchi tumani', 8),
+('Dehqonobod tumani', 8),
+('G''uzor tumani', 8),
+('Kasbi tumani', 8),
+('Kitob tumani', 8),
+('Koson tumani', 8),
+('Mirishkor tumani', 8),
+('Muborak tumani', 8),
+('Nishon tumani', 8),
+('Qarshi tumani', 8),
+('Shahrisabz tumani', 8),
+('Yakkabog'' tumani', 8);
+
+-- ═══ 9. SURXONDARYO VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Termiz shahri', 9),
+('Angor tumani', 9),
+('Boysun tumani', 9),
+('Denov tumani', 9),
+('Jarqo''rg''on tumani', 9),
+('Muzrabot tumani', 9),
+('Oltinsoy tumani', 9),
+('Qiziriq tumani', 9),
+('Qumqo''rg''on tumani', 9),
+('Sariosiyo tumani', 9),
+('Sherobod tumani', 9),
+('Sho''rchi tumani', 9),
+('Termiz tumani', 9),
+('Uzun tumani', 9);
+
+-- ═══ 10. JIZZAX VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Jizzax shahri', 10),
+('Arnasoy tumani', 10),
+('Baxmal tumani', 10),
+('Do''stlik tumani', 10),
+('Forish tumani', 10),
+('G''allaorol tumani', 10),
+('Mirzacho''l tumani', 10),
+('Paxtakor tumani', 10),
+('Sharof Rashidov tumani', 10),
+('Yangiobod tumani', 10),
+('Zafarobod tumani', 10),
+('Zarbdor tumani', 10),
+('Zomin tumani', 10);
+
+-- ═══ 11. SIRDARYO VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Guliston shahri', 11),
+('Shirin shahri', 11),
+('Yangiyer shahri', 11),
+('Boyovut tumani', 11),
+('Guliston tumani', 11),
+('Xovos tumani', 11),
+('Mirzaobod tumani', 11),
+('Oqoltin tumani', 11),
+('Sardoba tumani', 11),
+('Sayxunobod tumani', 11);
+
+-- ═══ 12. NAVOIY VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Navoiy shahri', 12),
+('Zarafshon shahri', 12),
+('Karmana tumani', 12),
+('Konimex tumani', 12),
+('Navbahor tumani', 12),
+('Nurota tumani', 12),
+('Qiziltepa tumani', 12),
+('Tomdi tumani', 12),
+('Uchquduq tumani', 12),
+('Xatirchi tumani', 12);
+
+-- ═══ 13. XORAZM VILOYATI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Urganch shahri', 13),
+('Xiva shahri', 13),
+('Bog''ot tumani', 13),
+('Gurlan tumani', 13),
+('Shovot tumani', 13),
+('Xonqa tumani', 13),
+('Hazorasp tumani', 13),
+('Qo''shko''pir tumani', 13),
+('Urganch tumani', 13),
+('Yangiariq tumani', 13),
+('Yangibozor tumani', 13);
+
+-- ═══ 14. QORAQALPOG'ISTON RESPUBLIKASI ═══
+INSERT INTO districts (name, province_id) VALUES
+('Nukus shahri', 14),
+('Amudaryo tumani', 14),
+('Beruniy tumani', 14),
+('Chimboy tumani', 14),
+('Ellikqal''a tumani', 14),
+('Kegeyli tumani', 14),
+('Mo''ynoq tumani', 14),
+('Nukus tumani', 14),
+('Qanliko''l tumani', 14),
+('Qo''ng''irot tumani', 14),
+('Qorao''zak tumani', 14),
+('Shumanay tumani', 14),
+('Taxtako''pir tumani', 14),
+('To''rtko''l tumani', 14),
+('Xo''jayli tumani', 14);
